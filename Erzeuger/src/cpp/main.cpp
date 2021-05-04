0388158c78bf0894d8e67470b2383ef7808eaad8
pm <philipp@family-meier.de>
2021-05-04 22:36:23 +0200

    minor edit

--- Erzeuger/src/cpp/main.cpp
+++ Erzeuger/src/cpp/main.cpp
@@ -12,16 +12,8 @@ int main() {
     srand(time(NULL));
     Erzeuger* erzeuger = new Solar(123, "aaa", 1);
 
-    for (int i = 0; i < 365; ++i){
-        for (int j = 0; j < 24; ++j){
-            double cons = erzeuger->getLastHourGeneration();
-            std::cout << cons << " kW/h" << std::endl;
-        }
-    }
     Simulator* sim = new Simulator(erzeuger,"UDP",5000,"127.0.0.1");
     sim->start();
-
-    std::cout << "Complete: " << erzeuger->getCompleteGeneration();
 
     delete erzeuger;
     return 0;
