44de1161b4da763793d91bf681145424bee72371
pm <philipp@family-meier.de>
2021-05-19 18:07:21 +0200

    implemented args and changed docker-compose.yml

--- Verbraucher/src/cpp/main.cpp
+++ Verbraucher/src/cpp/main.cpp
@@ -4,6 +4,7 @@
 #include "../header/Verbraucher.h"
 #include "../header/Unternehmen.h"
 #include "../header/Simulator.h"
+#include "../header/Haushalt.h"
 
 /**
  * Global variable
@@ -37,13 +38,54 @@ int main(int argc, char* args[]) {
     // Check for ip and port
     if (!argsMap.contains("ip") || !argsMap.contains("port")) {
         cout << "Ziel-Ip (ip=...) und Ziel-Port (port=...) muss gegeben sein." << endl;
-        return 1;
+        return -1;
     }
+
+    // check for id
+    int id;
+    auto iter = argsMap.find("id");
+    if (iter == argsMap.end()) {
+        cout << "ID fehlt!" << endl;
+        return -1;
+    } else
+        id = stoi(iter->second);
+
+    //check for name
+    string name;
+    iter = argsMap.find("name");
+    if (iter == argsMap.end()) {
+        cout << "Name fehlt!" << endl;
+        return -1;
+    } else
+        name = iter->second;
+
+    //check for communication type
+    string comType;
+    iter = argsMap.find("communication");
+    if (iter == argsMap.end()) {
+        cout << "Kommunikationstyp fehlt!" << endl;
+        return -1;
+    } else
+        comType = iter->second;
 
     // Verbraucher erstellen
     srand(time(NULL));
-    Verbraucher* verbraucher = new Unternehmen(123, "FLEISCHER");
-    sim = new Simulator(verbraucher, "UDP", stoi(argsMap.at("port")), argsMap.at("ip"));
+    Verbraucher* verbraucher = nullptr;
+    auto it = argsMap.find("type");
+    if (it != argsMap.end()){
+        if (it->second == "Haushalt")
+            verbraucher = new Haushalt(id, name);
+        else if (it->second == "Unternehmen")
+            verbraucher = new Unternehmen(id, name);
+        else {
+            cout << "Fehlerhafter Verbrauchertyp: " << it->second << endl;
+            return -1;
+        }
+    } else {
+        cout << "Verbraucher Typ muss gegeben sein" << endl;
+        return -1;
+    }
+    sim = new Simulator(verbraucher, comType, stoi(argsMap.at("port")), argsMap.at("ip"));
 
     // Signal handler mit SIGTERM
     signal(SIGTERM, sigTermHandler);
