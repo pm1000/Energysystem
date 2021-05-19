28b5bedd39b7e39fc1b51dab5b8fa8b618233f23
pm <philipp@family-meier.de>
2021-05-19 18:45:23 +0200

    whooops forgot one file

---
+++ Erzeuger/src/header/Simulator.h
@@ -0,0 +1,42 @@
+//
+// Created by philipp on 04.05.21.
+//
+
+#ifndef ERZEUGER_SIMULATOR_H
+#define ERZEUGER_SIMULATOR_H
+
+#include "Komponente/Erzeuger.h"
+#include "iostream"
+#include "unistd.h"
+#include "thread"
+#include "UDP/UDPKommunikation.h"
+#include "ZentralenKommunikation.h"
+#include "unordered_map"
+
+using namespace std;
+
+/**
+ *
+ */
+class Simulator {
+private:
+    unordered_map<int,string> msgBuffer;
+    Erzeuger* erzeuger;
+    UDPKommunikation* interface;
+    string messageToJSON(string type, string name, int id, double value, unsigned long long t);
+    static int msgID;
+
+    //helper methods
+    void simulate();
+    bool stopped = false;
+
+public:
+    Simulator(Erzeuger *erzeuger, string communicationType, int port, string address);
+    virtual ~Simulator();
+
+    void start();
+    void stop();
+};
+
+
+#endif //ERZEUGER_SIMULATOR_H
