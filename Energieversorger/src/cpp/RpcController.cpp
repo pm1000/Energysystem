d97fbaa5a9b939c4012746463c67bf18d63875a2
pm <philipp@family-meier.de>
2021-06-09 13:42:26 +0200

    rpc installed

--- Energieversorger/src/cpp/RpcController.cpp
+++ Energieversorger/src/cpp/RpcController.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "../header/RpcController.h"
+
+RpcController::RpcController() = default;
+
+RpcController::~RpcController() = default;
+
+void RpcController::init(string ipAddress, int port) {
+    try {
+        string address = ipAddress + ":" + std::to_string(port);
+        auto channel = grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
+        stub = Energieversorger::KomponentenData::NewStub(channel);
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
+
+vector<Komponente *> RpcController::getKomponenten() {
+    return vector<Komponente *>();
+}
+
+Komponente *RpcController::getKomponentenData(int id) {
+    return nullptr;
+}
+
+vector<int> RpcController::getKomponentenIDs(const Energieversorger::KomponentenIDs &komponentenIDs) {
+    
+
+    return vector<int>();
+}
--- Energieversorger/src/header/RpcController.h
+++ Energieversorger/src/header/RpcController.h
@@ -4,16 +4,31 @@
 
 #ifndef ENERGIEVERSORGER_RPCCONTROLLER_H
 #define ENERGIEVERSORGER_RPCCONTROLLER_H
-#include "../../generated/server.pb.h"
+#include "../../generated/energieversorger.pb.h"
 #include "string"
+#include "vector"
+#include "Komponenten/Komponente.h"
+#include "map"
+#include <grpcpp/create_channel.h>
+#include <energieversorger.grpc.pb.h>
 
 using std::string;
+using std::vector;
+using std::map;
 
 class RpcController {
-private:
-    string ipAddress;
-    int port;
+    std::unique_ptr<Energieversorger::KomponentenData::Stub> stub;
 
+    vector<int> getKomponentenIDs(const Energieversorger::KomponentenIDs& komponentenIDs);
+    Komponente* getKomponentenData(int id);
+
+public:
+    RpcController();
+    virtual ~RpcController();
+
+    void init(string ipAddress, int port);
+
+    vector<Komponente*> getKomponenten();
 };
 
 
